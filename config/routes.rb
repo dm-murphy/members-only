223e66947d7fd9e21f97ff473a808a0c6707f2ea
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-07-19 17:03:50 -0400

    Generate model for story and update app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "stories#index"
-  
-  get "/stories", to: "stories#index" 
+
+  get "/stories", to: "stories#index"
+  get "/stories/:id", to: "stories#show"
 end
