58e72c5696bc42b9a9e57592171c13a81902bcff
Daniel Murphy <danielmichaelmurphy@gmail.com>
2021-07-19 17:20:10 -0400

    Add create and new methods

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -6,4 +6,18 @@ class StoriesController < ApplicationController
   def show
     @story = Story.find(params[:id])
   end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(title: "...", body: "...")
+
+    if @story.save
+      redirect_to @story
+    else
+      render :new
+    end
+  end
 end
